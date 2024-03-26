49431a02c7bdeb798da41b6451eb0978ae0348cf
Satishghode <satishghode9075@gmail.com>
2024-03-27 00:00:16 +0530

    Remove Element

--- src/array/RemoveEleent.java
+++ src/array/RemoveEleent.java
@@ -17,24 +17,29 @@ public class RemoveEleent {
 		System.out.print("Enter the Array size : ");
 		int[] arr = new int[scanner.nextInt()];
 		
-		System.out.print("Ente value to remove array: ");
-		int value = scanner.nextInt();
+		
 		
 		for(int i = 0 ; i < arr.length ; i++) {
 			arr[i] = scanner.nextInt();
 		}
+		System.out.print("Ente value to remove array: ");
+		int value = scanner.nextInt();
 		
 		int ans = (int) removeElement(arr, value)[0];
 		int[] ansArr = (int[]) removeElement(arr, value)[1]; 
 		
 		System.out.println(ans);
+		
+//		for (int j = 0; j < ansArr.length; j++) {
+//			System.out.print(ansArr[j] + " ");
+//		}
 
 	}
 	static Object[] removeElement(int[] arr, int value) {
 		Object result[] = new Object[2];
 		
 		int count = 0 ; 
-		int ans[] = new int[count];
+		int ans[] = new int[arr.length];
 		for(int i=0 ; i<arr.length ; i++ ) {
 			if(arr[i] != value) {
 				ans[count] = arr[i];
